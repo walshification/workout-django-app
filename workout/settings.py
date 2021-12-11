b7b55dcf3146c4cdd1549593c059d31363abc95f
Christopher Walsh <5120876+walshification@users.noreply.github.com>
2021-12-11 13:46:08 -0600

    Establish TZ and create register/login/logout urls

--- workout/settings.py
+++ workout/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "exercises.apps.ExercisesConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "exercises",
 ]
 
 MIDDLEWARE = [
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "America/Chicago"
 
 USE_I18N = True
 
--- workout/urls.py
+++ workout/urls.py
@@ -14,9 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import include, path
 
+from exercises.views import Register
+
 urlpatterns = [
+    path("accounts/register/", Register.as_view(), name="register"),
+    path("accounts/login/", LoginView.as_view(), name="login"),
+    path("accounts/logout/", LogoutView.as_view(), name="logout"),
     path("exercises/", include("exercises.urls")),
     path("admin/", admin.site.urls),
 ]
