2a08ca5b1bcc02b9f94c6f8b5e0846aaec774837
Christopher Walsh <5120876+walshification@users.noreply.github.com>
2021-12-11 13:48:18 -0600

    Allow registration and logging in

--- exercises/views.py
+++ exercises/views.py
@@ -1,5 +1,26 @@
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse
+from django.urls import reverse_lazy
+from django.views.generic.edit import FormView
 
 
+class Register(FormView):
+    """Sign up!"""
+
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy("exercises:index")
+    template_name = "register.html"
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return super().form_valid(form)
+
+
+@login_required
 def index(request):
     return HttpResponse("Hello, world. You're at the exercises index.")
