dab41db9f759a51d2abcfb5ce50509f0ee4534e2
Christopher Walsh <5120876+walshification@users.noreply.github.com>
2021-12-16 20:44:17 -0600

    Juggle active workouts

--- exercises/urls.py
+++ exercises/urls.py
@@ -9,4 +9,9 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("routines", views.RoutineList.as_view(), name="routines_list"),
     path("create", views.CreateWorkout.as_view(), name="create_workout"),
+    path("active", views.UpdateWorkout.as_view(), name="active_workout"),
+    path("history", views.index, name="history"),
+    path(
+        "<slug:exercise_id>/sets/create", views.CreateSet.as_view(), name="create_set"
+    ),
 ]
--- exercises/views.py
+++ exercises/views.py
@@ -6,12 +6,12 @@ from django.contrib.auth.models import User
 from django.db.models import QuerySet
 from django.shortcuts import redirect, render
 from django.urls import reverse, reverse_lazy
-from django.views.generic.edit import CreateView
-from django.views.generic.edit import FormView
+from django.utils import timezone
+from django.views.generic.edit import CreateView, FormView
 from django.views.generic.list import ListView
 
-# from exercises.forms import NewWorkoutForm
-from exercises.models import Routine, Workout
+from exercises.forms import CreateWorkoutForm, UpdateWorkoutForm
+from exercises.models import Exercise, Routine, Set, Workout
 
 
 class Register(FormView):
@@ -49,20 +49,94 @@ class RoutineList(LoginRequiredMixin, ListView):
         return Routine.objects.filter(owner=self.request.user)
 
 
-class CreateWorkout(LoginRequiredMixin, CreateView):
+class CreateWorkout(LoginRequiredMixin, FormView):
     """Start a new workout for a given routine."""
 
-    model = Workout
-    fields = ["routine"]
+    form_class = CreateWorkoutForm
+    template_name = "exercises/workout_form.html"
+    success_url = reverse_lazy("exercises:active_workout")
+
+    def get_form(self, *args, **kwargs):
+        routines = self.request.user.routines.all()
+        form = super().get_form(*args, **kwargs)
+        form.fields["routine"].choices = [("", "--------")] + list(
+            (routine.name, routine.name) for routine in routines
+        )
+        return form
+
+    def form_valid(self, form):
+        """Verify we have a routine to make the workout out of."""
+        if form.is_valid():
+            Workout.objects.create(
+                routine=Routine.objects.create()
+            )
+            form.instance.owner = self.request.user
+            redirect(reverse("exercises:active_workout"))
+        return render(
+            self.request,
+            reverse("exercises:create_workout"),
+            {"form": form},
+        )
+
+
+class UpdateWorkout(LoginRequiredMixin, FormView):
+    """Update the workout with the sets done."""
+
+    form_class = UpdateWorkoutForm
+    template_name = "exercises/active_workout.html"
+    success_url = reverse_lazy("exercises:history")
+
+    def get_context_data(self, **kwargs):
+        """Prep data for the form."""
+        data = super().get_context_data(**kwargs)
+        data["exercises"] = []
+        workout = Workout.objects.filter(
+            owner=self.request.user, is_completed=False
+        ).first()
+        for exercise in workout.routine.exercises.all():
+            data["exercises"].append(exercise)
+        return data
+
+    def form_valid(self, form):
+        """Update!"""
+        if form.is_valid():
+            workout = Workout.objects.filter(
+                owner=self.request.user, is_completed=False
+            ).first()
+            workout.completed_at = timezone.now()
+            workout.is_completed = True
+            workout.save()
+            return super().form_valid(form)
+
+        return render(
+            self.request,
+            reverse("exercises:active_workout"),
+            {"form": form},
+        )
+
+    def form_invalid(self, form):
+        return super().form_invalid(form)
+
+
+class CreateSet(LoginRequiredMixin, CreateView):
+    """Start a new set for a given exercise."""
+
+    model = Set
+    fields = ["weight", "reps"]
     success_url = reverse_lazy("exercises:active_workout")
 
     def form_valid(self, form):
         """Verify we have a routine to make the workout out of."""
         if form.is_valid():
-            form.instance.owner = self.request.user
-            return super().form_valid(form)
+            Exercise.objects.get(pk=int(self.kwargs["exercise_id"])).sets.add(
+                Set.objects.create(
+                    weight=form.cleaned_data["weight"],
+                    reps=form.cleaned_data["reps"],
+                )
+            )
+            return redirect(reverse("exercises:active_workout"))
         return render(
             self.request,
-            reverse("exercises:create_workout"),
+            reverse("exercises:create_set"),
             {"form": form},
         )
